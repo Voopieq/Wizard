b9ce20548dece05e22a344188f4e78931e30d3db
Voopieq <domantas.kelmanas2003@gmail.com>
2024-09-14 22:52:33 +0300

    Grid System
    -Actual Hex sprites appear and form hex map
    -Hexes detect mouse position, but incorrectly. (Fix this later)

--- Wizard/Assets/Scripts/GridSystem/TestingGrid.cs
+++ Wizard/Assets/Scripts/GridSystem/TestingGrid.cs
@@ -4,33 +4,61 @@ using UnityEngine;
 
 public class TestingGrid : MonoBehaviour
 {
-    private Grid _grid;
+    private GridHex<GridObject> _grid;
     private Camera _camera;
-    private Vector2 _mousePos;
+    private Vector3 _mousePos;
 
-    [SerializeField] Transform pfSquare;
+    [SerializeField] Transform prefabHex;
+    private GridObject _gridObject;
+
+    private class GridObject
+    {
+        public Transform cloneTransform;
+
+        public void Show()
+        {
+            cloneTransform.Find("Selected").gameObject.SetActive(true);
+        }
+
+        public void Hide()
+        {
+            cloneTransform.Find("Selected").gameObject.SetActive(false);
+        }
+    }
 
     void Start()
     {
-        _grid = new Grid(4, 2, 10f);
+        _grid = new GridHex<GridObject>(8, 5, 1f, (GridHex<GridObject> g, int x, int y) => new GridObject());
         _camera = Camera.main;
+
+        for (int x = 0; x < _grid.GetWidth(); x++)
+        {
+            for (int y = 0; y < _grid.GetHeight(); y++)
+            {
+                Transform cloneTransform = Instantiate(prefabHex, _grid.GetWorldPosition(x, y), Quaternion.identity);
+                _grid.GetHexValue(x, y).cloneTransform = cloneTransform;
+                _grid.GetHexValue(x, y).Hide();
+            }
+        }
     }
+
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if(_gridObject != null)
         {
-            _mousePos = GetMouseWorldPos(Input.mousePosition);
-            _grid.SetValue(_mousePos, 15);
+            _gridObject.Hide();
         }
-        if(Input.GetMouseButtonDown(1))
+        _mousePos = GetMouseWorldPos(Input.mousePosition);
+        GridObject gridObject = _grid.GetHexValue(_mousePos);
+        if (gridObject != null)
         {
-            _mousePos = GetMouseWorldPos(Input.mousePosition);
-            Debug.Log(_grid.GetValue(_mousePos));
+            gridObject.Show();
         }
+
     }
 
-    private Vector2 GetMouseWorldPos(Vector3 mousePos)
+    private Vector3 GetMouseWorldPos(Vector3 mousePos)
     {
         return _camera.ScreenToWorldPoint(mousePos);
     }
